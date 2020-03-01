b27b1df2a3b70a0c748b312c905af8a2499b13a5
banlangen <wnh_008@163.com>
2020-03-02 00:47:18 +0800

    update CMakeList.txt to make it compatible with qcc

--- src/test.cpp
+++ src/test.cpp
@@ -42,18 +42,18 @@ class AddFixture {
 boost::shared_ptr<AddFixture> aFixture (new AddFixture);
 
 BOOST_AUTO_TEST_SUITE(suite)
-/*BOOST_FIXTURE_TEST_CASE(record_screen_position_dump, AddFixture) {
+BOOST_FIXTURE_TEST_CASE(record_screen_position_dump, AddFixture) {
     BOOST_TEST_MESSAGE("Running RecordTest");
     BOOST_TEST_MESSAGE("  recording position 1");
     ScreenPosition sp; 
-    sp.record(12,27,89);
-    outfile << sp ;// dont put << endl here.
+    sp.record(12,2,27,89);
+    outfile << sp ;// please dont put << endl here.
     BOOST_TEST_MESSAGE("  Sleeping for 2 seconds");
     std::this_thread::sleep_for(std::chrono::seconds(2));
     BOOST_TEST_MESSAGE("  recording position 2");
-    sp.record(12,89,78);
+    sp.record(12,2,89,78);
     outfile << sp ;
-}*/
+}
 BOOST_AUTO_TEST_CASE(fixture_demo0, * fixture(boost::bind(&AddFixture::openConfigFile, aFixture), boost::bind(&AddFixture::closeConfigFile, aFixture))) {
     BOOST_TEST_MESSAGE("Running ReadTest");
     ScreenPosition sp;
